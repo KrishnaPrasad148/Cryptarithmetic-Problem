d47deb144e429d3863a0f504f5d1f737f84507f0
KrishnaPrasad148 <krishnaprasadsk141@gmail.com>
2026-08-17 14:02:47 +0530

    Update

--- csp.py
+++ csp.py
@@ -14,8 +14,14 @@ def solve_cryptarithmetic():
         S, E, N, D, M, O, R, Y = perm
 
         # Check for leading zeros
-        TYPE THE CODE HERE USING IF
+        if S == 0 or M == 0:
             continue
+
+        # Check the equation constraints
+        SEND = 1000 * S + 100 * E + 10 * N + D
+        MORE = 1000 * M + 100 * O + 10 * R + E
+        MONEY = 10000 * M + 1000 * O + 100 * N + 10 * E + Y
+
 
         # Check the equation constraints
         SEND = 1000 * S + 100 * E + 10 * N + D
@@ -27,7 +33,7 @@ def solve_cryptarithmetic():
 
     return None
 
-solution = TYPE THE CODE HERE #CALL THE FUNCTION
+solution = solve_cryptarithmetic() #CALL THE FUNCTION
 
 if solution:
     SEND, MORE, MONEY = solution
